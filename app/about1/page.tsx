0bd6e40ab3028037bcec12582c7f6b13d01bfeff
lovabadgal <167182513+lovabadgal@users.noreply.github.com>
2026-07-06 23:54:21 +0300

    Update page.tsx

--- app/about1/page.tsx
+++ app/about1/page.tsx
@@ -3,7 +3,7 @@ const About = () => {
         <div>
         
             <h1>About</h1>
-            <p>Bu benim ana sayfamın ilk paragrafı. Next.js öğreniyorum!</p>
+            <p>Bu benim ana sayfamın ilk paragrafı. Next.js öğreniyorum</p>
             <p>Buraya da ikinci paragrafı ekledim, tıkır tıkır çalışıyor.</p>
         </div>
     );
