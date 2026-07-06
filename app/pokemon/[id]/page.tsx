380a1ee19c96c0267f704018a490faeaba00eebe
lovabadgal <167182513+lovabadgal@users.noreply.github.com>
2026-07-06 23:53:38 +0300

    Update page.tsx

--- app/pokemon/[id]/page.tsx
+++ app/pokemon/[id]/page.tsx
@@ -1,17 +1,17 @@
 import Link from 'next/link';
 export default async function PokemonPage({ params }: any) {
-  // params'ı await ile açıyoruz
+  
   const { id } = await params; 
 
   const url = `https://pokeapi.co/api/v2/pokemon/${id.toLowerCase()}`;
-  // ... gerisi aynı!
+  
   console.log("İstek atılan adres:", url);
 
   const res = await fetch(url);
   
   
   if (!res.ok) {
-    return <div>Bu Pokémon verisine ulaşılamadı. Lütfen tekrar dene.</div>;
+    return <div>This Pokémon data could not be accessed. Please try again later.</div>;
   }
 
   const pokemon = await res.json();
